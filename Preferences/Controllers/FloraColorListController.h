36e3ebd7fe6b2dce2329f90dd572740fb092cf64
Rosie <acquitefx@gmail.com>
2024-01-16 13:57:31 +0000

    Implement ability to search for specifiers

--- Preferences/Controllers/FloraColorListController.h
+++ Preferences/Controllers/FloraColorListController.h
@@ -2,7 +2,9 @@
 #import <Preferences/Preferences.h>
 #import "../../Tweak/Utilities.h"
 
-@interface FloraColorListController : PSListController
+@interface FloraColorListController : PSListController <UISearchBarDelegate>
+@property (nonatomic, assign) NSUInteger lastSearchBarTextLength;
+
 - (PSSpecifier *)generateSpecifierWithName:(NSString *)name parsedName:(NSString *)parsedName hexColor:(NSString *)hexColor;
 - (NSString *)parseName:(NSString *)name;
 - (NSMutableArray *)getColorSpecifiersWithFilter:(BOOL (^)(NSString *name))filter parser:(NSString *(^)(NSString *name))parser;
--- Preferences/Controllers/FloraColorListController.m
+++ Preferences/Controllers/FloraColorListController.m
@@ -2,6 +2,58 @@
 
 @implementation FloraColorListController
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    UISearchController *searchController = [[UISearchController alloc] initWithSearchResultsController:nil];
+    searchController.definesPresentationContext = YES;
+    searchController.hidesNavigationBarDuringPresentation = YES;
+    searchController.searchBar.delegate = self;
+    searchController.obscuresBackgroundDuringPresentation = NO;
+
+    [((FloraColorListController *)self).navigationItem setSearchController:searchController];
+    [((FloraColorListController *)self).navigationItem setHidesSearchBarWhenScrolling:YES];
+    [self setLastSearchBarTextLength:0];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    
+    [self reloadSpecifiers];
+    [((FloraColorListController *)self).navigationItem.searchController setActive:NO];
+    [self setLastSearchBarTextLength:0];
+}
+
+- (void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar {
+    [searchBar setShowsCancelButton:YES animated:YES];
+}
+
+- (void)searchBarTextDidEndEditing:(UISearchBar *)searchBar {
+    [searchBar setShowsCancelButton:NO animated:YES];
+}
+
+-(void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)text {
+    if([text length] < [self lastSearchBarTextLength]) {  //by p0358
+        [self reloadSpecifiers];
+    }
+
+    if([text length] > 0) {
+        for(PSSpecifier *specifier in [self valueForKey:@"_specifiers"]) {
+            NSRange titleRange = [specifier.name rangeOfString:text options:NSCaseInsensitiveSearch];
+            if([specifier.name length] > 0 && titleRange.location == NSNotFound) {
+                [self removeSpecifier:specifier];
+            }
+        }
+    }
+
+    [self setLastSearchBarTextLength:[text length]];
+}
+
+-(void)searchBarCancelButtonClicked:(UISearchBar *)searchBar {
+    [self reloadSpecifiers];
+    [self setLastSearchBarTextLength:0];
+}
+
 - (PSSpecifier *)generateSpecifierWithName:(NSString *)name parsedName:(NSString *)parsedName hexColor:(NSString *)hexColor {
     PSSpecifier *specifier = [PSSpecifier preferenceSpecifierNamed:parsedName
                                                             target:self
